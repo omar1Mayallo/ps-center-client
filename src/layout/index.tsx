6bc5c878f478be70c330aca91594720466bb06e6
Omar Mayallo <106473708+omar1Mayallo@users.noreply.github.com>
2023-06-26 03:45:29 +0300

    Build pages skeleton

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,13 +1,36 @@
 import {Outlet} from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
+import {Container, Grid, Theme, useMediaQuery} from "@mui/material";
+import Sidebar from "./Sidebar";
 
 export default function Layout() {
+  const isLargeScreen: boolean = useMediaQuery<Theme>((theme) =>
+    theme.breakpoints.up("md")
+  );
+
   return (
     <>
-      <header>Header</header>
-      <main id="main">
-        <Outlet />
-      </main>
-      <footer>Footer</footer>
+      <Header />
+      <Container maxWidth="xl">
+        {isLargeScreen ? (
+          <Grid container spacing={2}>
+            <Grid item lg={2}>
+              <Sidebar />
+            </Grid>
+            <Grid item lg={10}>
+              <main>
+                <Outlet />
+              </main>
+            </Grid>
+          </Grid>
+        ) : (
+          <main>
+            <Outlet />
+          </main>
+        )}
+      </Container>
+      <Footer />
     </>
   );
 }
